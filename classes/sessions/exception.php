7c3589df9e2583a59edbfb4874adec96621433b4
MichalM <michal.j.musial@gmail.com>
2012-11-30 19:38:42 +0100

    Sessions_Exception now extends Session_Exception

--- classes/sessions/exception.php
+++ classes/sessions/exception.php
@@ -7,9 +7,6 @@
  * @category   Exceptions
  * @author     Kohana Team, Michał Musiał
  * @copyright  Original code (c) 2008-2011 Kohana Team, modifications (c) 2012 Michał Musiał
- * @license    http://kohanaframework.org/licenselicense
+ * @license    http://kohanaframework.org/license
  */
-class Sessions_Exception extends Kohana_Exception
-{
-	const SESSION_CORRUPT = 1;
-}
+class Sessions_Exception extends Session_Exception {}
